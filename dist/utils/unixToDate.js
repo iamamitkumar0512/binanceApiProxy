e9d5b365c81251dd74d5e4e8251ee0ff3230b63a
Amit Kumar <iamamitkumar051@gmail.com>
2025-02-22 00:27:09 +0530

    Removed dist  from repository

--- dist/utils/unixToDate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertUnixToDateTime = convertUnixToDateTime;
-function convertUnixToDateTime(unixTimestamp) {
-    const date = new Date(unixTimestamp);
-    return date.toISOString(); // Format as "YYYY-MM-DDTHH:mm:ss.sssZ"
-}
